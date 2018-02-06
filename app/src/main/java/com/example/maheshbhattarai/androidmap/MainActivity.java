6e61e1cb58501511a9b3f5ea91f4b4bc8ee14716
Mahesh <mkbhattarai1@gmail.com>
2018-02-06 16:40:07 -0700

    Merge remote-tracking branch 'Track_Your_Stuff/master'
    # Conflicts:
    #	app/src/main/java/com/example/maheshbhattarai/androidmap/MainActivity.java

--- app/src/main/java/com/example/maheshbhattarai/androidmap/MainActivity.java
+++ app/src/main/java/com/example/maheshbhattarai/androidmap/MainActivity.java
@@ -40,11 +40,11 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.List;
 
-// my first comment
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         com.google.android.gms.location.LocationListener
-//here mention all interface need implement
+
 {
 
 
@@ -74,15 +74,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //this is for version permission
+        // version permission
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //here this condition check for android version
-            //before m->march melo version , if you are running android version before march melon ,
-            //then you don't require permission
-            //but if you are running later version after march melo
-            //then you need to check permssion before loading map.
-            checkLocationPermission(); //This is method for getting permission
-            //will go to there
+
+            checkLocationPermission();
+
         }
 
         first_address = (EditText) findViewById(R.id.first_address);
@@ -93,18 +89,69 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         bnt_findMiles = (Button) findViewById(R.id.show_distance);
 
 
-        //this is used for loading map in desing
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_image); //map_img id for fragment map, laready there in desing
+                .findFragmentById(R.id.map_image);
 
-        //this is loading map
-        mapFragment.getMapAsync(this); //this will generate onMapReady method , is below
+        // loading map
+        mapFragment.getMapAsync(this);
 
 
         bnt_findMiles.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mMap.clear();
+                /*String strAddress =
+                String strAddress2 =
+                 Geocoder coder = new Geocoder(MainActivity.this);
+                List<Address> latlonh_address = null;
+
+                try {
+                    // throw an IOException
+                    latlonh_address = coder.getFromLocationName(strAddress, 50);
+                    if (latlonh_address == null) {
+                        Toast.makeText(MainActivity.this, "first addres null", Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Address location = latlonh_address.get(0);
+                        currentLatitute = latlonh_address.get(0).getLatitude();
+                        currentLongtitute = latlonh_address.get(0).getLongitude();
+                        Log.e("latlong : ", "lat" + currentLatitute + " long" + currentLongtitute);
+
+                        //showMiless.setText("Latitude : " + location.getLatitude() + "\n" + "Longtitude :" + location.getLongitude());
+
+                    }
+
+                } catch (IOException ex) {
+
+                    ex.printStackTrace();
+                }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+                Geocoder coder2 = new Geocoder(MainActivity.this);
+                List<Address> latlonh_address2 = null;
+
+                try {
+                    // May throw an IOException
+                    latlonh_address2 = coder2.getFromLocationName(strAddress2, 50);
+                    if (latlonh_address2 == null) {
+                        Toast.makeText(MainActivity.this, "first addres null", Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Address location = latlonh_address.get(0);
+                        destinationLatitute = latlonh_address2.get(0).getLatitude();
+                        destinationLongtitute = latlonh_address2.get(0).getLongitude();
+                        Log.e("latlong2 : ", "lat" + destinationLatitute + " long" + destinationLongtitute);
+
+                        //showMiless.setText("Latitude : " + location.getLatitude() + "\n" + "Longtitude :" + location.getLongitude());
+
+                    }
+
+                } catch (IOException ex) {
+
+                    ex.printStackTrace();
+                }*/
+
 
                 if (first_address.getText().toString().isEmpty()) {
                     Toast.makeText(MainActivity.this, "Enter Location Address first", Toast.LENGTH_SHORT).show();
@@ -114,42 +161,24 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     String strAddress = first_address.getText().toString();
                     String strAddress2 = second_address.getText().toString();
 
+                    /*String strAddress = "1198 11th Street, Denver, CO 80204";
+                    String strAddress2 = "8500 Pena Blvd Denver, CO 80249";*/ //
 
-                    Geocoder coder2 = new Geocoder(MainActivity.this);
-                    List<Address> latlonh_address2;
-
-                    try {
-                        // May throw an IOException
-                        latlonh_address2 = coder2.getFromLocationName(strAddress2, 5);
-                        if (latlonh_address2 == null) {
-                            Toast.makeText(MainActivity.this, "second addres null", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Address location = latlonh_address2.get(0);
-                            destinationLatitute = location.getLatitude();
-                            destinationLongtitute = location.getLongitude();
-
-                            //showMiless.setText("Latitude : " + location.getLatitude() + "\n" + "Longtitude :" + location.getLongitude());
-
-                        }
-
-                    } catch (IOException ex) {
-
-                        ex.printStackTrace();
-                    }
 
 
                     Geocoder coder = new Geocoder(MainActivity.this);
-                    List<Address> latlonh_address;
+                    List<Address> latlonh_address = null;
 
                     try {
-                        // May throw an IOException
-                        latlonh_address = coder.getFromLocationName(strAddress, 5);
+                        // throw an IOException
+                        latlonh_address = coder.getFromLocationName(strAddress, 50);
                         if (latlonh_address == null) {
                             Toast.makeText(MainActivity.this, "first addres null", Toast.LENGTH_SHORT).show();
                         } else {
-                            Address location = latlonh_address.get(0);
-                            currentLatitute = location.getLatitude();
-                            currentLongtitute = location.getLongitude();
+                            //Address location = latlonh_address.get(0);
+                            currentLatitute = latlonh_address.get(0).getLatitude();
+                            currentLongtitute = latlonh_address.get(0).getLongitude();
+                            Log.e("latlong : ", "lat" + currentLatitute + " long" + currentLongtitute);
 
                             //showMiless.setText("Latitude : " + location.getLatitude() + "\n" + "Longtitude :" + location.getLongitude());
 
@@ -161,47 +190,78 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     }
 
 
-                    int Radius = 6371;// radius of earth in Km
-                    double lat1 = currentLatitute;
-                    double lat2 = destinationLatitute;
-                    double lon1 = currentLongtitute;
-                    double lon2 = destinationLongtitute;
-                    double dLat = Math.toRadians(lat2 - lat1);
-                    double dLon = Math.toRadians(lon2 - lon1);
-                    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-                            + Math.cos(Math.toRadians(lat1))
-                            * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
-                            * Math.sin(dLon / 2);
-                    double c = 2 * Math.asin(Math.sqrt(a));
-                    double valueResult = Radius * c;
-                    double km = valueResult / 1;
-                    DecimalFormat newFormat = new DecimalFormat("####");
-                    int kmInDec = Integer.valueOf(newFormat.format(km));
-                    double meter = valueResult % 1000;
-                    int meterInDec = Integer.valueOf(newFormat.format(meter));
+                    Geocoder coder2 = new Geocoder(MainActivity.this);
+                    List<Address> latlonh_address2 = null;
 
-                    //... Computation
-                    mi = km * 0.621;
-                    showMiless.setText("Miles is : " + mi);
-                    Log.e("Distanc in miles : ", mi + " Miles");
-                    Log.e("Radius Value", "" + valueResult + "   KM  " + kmInDec
-                            + " Meter   " + meterInDec);
+                    try {
+                        // throw an IOException
+                        latlonh_address2 = coder2.getFromLocationName(strAddress2, 50);
+                        if (latlonh_address2 == null) {
+                            Toast.makeText(MainActivity.this, "second addres null", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Address location2 = latlonh_address2.get(0);
+                            destinationLatitute = location2.getLatitude();
+                            destinationLongtitute = location2.getLongitude();
+                            Log.e("latlong2 : ", "lat" + destinationLatitute + " long" + destinationLongtitute);
+
+                            //showMiless.setText("Latitude : " + location.getLatitude() + "\n" + "Longtitude :" + location.getLongitude());
+
+                        }
+
+                    } catch (IOException ex) {
+
+                        ex.printStackTrace();
+                    }
 
 
 
-                    LatLng origin = new LatLng(currentLatitute, currentLongtitute);
-                    LatLng destination = new LatLng(destinationLatitute, destinationLongtitute);
-                    DrawRouteMaps.getInstance(MainActivity.this)
-                            .draw(origin, destination, mMap);
-                    DrawMarker.getInstance(MainActivity.this).draw(mMap, origin, R.drawable.map_pin, "");
-                    DrawMarker.getInstance(MainActivity.this).draw(mMap, destination, R.drawable.map_pin, "");
 
-                    LatLngBounds bounds = new LatLngBounds.Builder()
-                            .include(origin)
-                            .include(destination).build();
-                    Point displaySize = new Point();
-                    getWindowManager().getDefaultDisplay().getSize(displaySize);
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, displaySize.x, 180, 15));
+                    if (currentLatitute != null && currentLongtitute != null && destinationLatitute != null && destinationLongtitute != null) {
+                        int Radius = 6371;// radius of earth in Km
+                        double lat1 = currentLatitute;
+                        double lat2 = destinationLatitute;
+                        double lon1 = currentLongtitute;
+                        double lon2 = destinationLongtitute;
+                        double dLat = Math.toRadians(lat2 - lat1);
+                        double dLon = Math.toRadians(lon2 - lon1);
+                        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                                + Math.cos(Math.toRadians(lat1))
+                                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                                * Math.sin(dLon / 2);
+                        double c = 2 * Math.asin(Math.sqrt(a));
+                        double valueResult = Radius * c;
+                        double km = valueResult / 1;
+                        DecimalFormat newFormat = new DecimalFormat("####");
+                        int kmInDec = Integer.valueOf(newFormat.format(km));
+                        double meter = valueResult % 1000;
+                        int meterInDec = Integer.valueOf(newFormat.format(meter));
+
+                        //... Computation
+                        mi = km * 0.621;
+                        showMiless.setText("Miles is : " + mi);
+                        Log.e("Distanc in miles : ", mi + " Miles");
+                        Log.e("Radius Value", "" + valueResult + "   KM  " + kmInDec
+                                + " Meter   " + meterInDec);
+
+
+                        LatLng origin = new LatLng(currentLatitute, currentLongtitute);
+                        LatLng destination = new LatLng(destinationLatitute, destinationLongtitute);
+                        DrawRouteMaps.getInstance(MainActivity.this)
+                                .draw(origin, destination, mMap);
+                        DrawMarker.getInstance(MainActivity.this).draw(mMap, origin, R.drawable.map_pin, "");
+                        DrawMarker.getInstance(MainActivity.this).draw(mMap, destination, R.drawable.map_pin, "");
+
+                        LatLngBounds bounds = new LatLngBounds.Builder()
+                                .include(origin)
+                                .include(destination).build();
+                        Point displaySize = new Point();
+                        getWindowManager().getDefaultDisplay().getSize(displaySize);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, displaySize.x, 180, 15));
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "location not found", Toast.LENGTH_SHORT).show();
+                    }
+
 
                 }
 
@@ -212,14 +272,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
-    //this is map ready method
+    // map ready method
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;  //creating Map Instance
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); //setting normal view , there is diferent view , but we have normal
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); //setting normal view
         mMap.getUiSettings().setMyLocationButtonEnabled(false); // set false
         mMap.getUiSettings().setMapToolbarEnabled(false); //set false
-
 
 
         //Initialize Google Play Services
@@ -227,19 +286,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             if (ContextCompat.checkSelfPermission(MainActivity.this,
                     Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED) {
-                //this is for getting permission if version is greater than march melo
+
                 buildGoogleApiClient();
                 mMap.setMyLocationEnabled(true);
             }
         } else {
 
-            //this is for below march melon
+
             buildGoogleApiClient();
             mMap.setMyLocationEnabled(true);
         }
     }
 
-    //This is built in method , just copy paste
+    //This is built in method
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -250,19 +309,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
-    //This is for finding current locatin method
+    // finding current locatin method
     @Override
     public void onLocationChanged(Location location) {
 
         mLastLocation = location;
         if (mLastLocation != null) {
             Log.e("location", "lat " + mLastLocation.getLatitude() + " long " + mLastLocation.getLongitude());
-            Toast.makeText(MainActivity.this,"lat " + mLastLocation.getLatitude() + " long " + mLastLocation.getLongitude(),Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "lat " + mLastLocation.getLatitude() + " long " + mLastLocation.getLongitude(), Toast.LENGTH_LONG).show();
             currentLatitute = mLastLocation.getLatitude();
             currentLongtitute = mLastLocation.getLongitude();
 
             if (currentLatitute != null && currentLongtitute != null) {
-               
+
             }
 
         }
@@ -275,7 +334,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
-    //this is for connecting with google map
+    // connecting with google map
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
@@ -292,14 +351,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-    //just copy paste
+
     @Override
     public void onConnectionSuspended(int i) {
 
     }
 
 
-    //This is for permission checking for laoding map
+    // permission checking for laoding map
     public boolean checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
@@ -320,7 +379,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    //Just copy paster.
+
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
